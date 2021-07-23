12e13d3668244d5e2ac8fc1c4671a64aa1b2b92d
Vlad Ghetina <ghetinavlad@gmail.com>
2021-07-23 14:34:41 +0300

    done athlete app

---
+++ FavoriteAthlete/Athlete.swift
@@ -0,0 +1,21 @@
+//
+//  Athlete.swift
+//  Athlete
+//
+//  Created by Wolfpack Digital on 23.07.2021.
+//
+
+import Foundation
+
+struct Meal {
+    var name: String
+    var age: Int
+    var league: String
+    var team: String
+    
+    var description: String {
+        return "\(name) is \(age) years old and plays for the \(team) in the \(league)."
+    }
+
+}
+
