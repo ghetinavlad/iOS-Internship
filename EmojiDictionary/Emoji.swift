c9ad3d12eab486b21ef3b38f78e013d37b736be7
Vlad Ghetina <ghetinavlad@gmail.com>
2021-07-28 15:03:09 +0300

    forgot to add table view ex

---
+++ EmojiDictionary/Emoji.swift
@@ -0,0 +1,17 @@
+//
+//  Emoji.swift
+//  Emoji
+//
+//  Created by Wolfpack Digital on 28.07.2021.
+//
+
+import Foundation
+
+
+struct Emoji {
+    var symbol: String
+    var name: String
+    var description: String
+    var usage: String
+}
+
